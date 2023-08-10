78a508920371d63ca4953051f3c7202cb577bca2
ali-057 <athuli057@gmail.com>
2023-08-10 14:55:27 +0000

    hh

--- hello.java
+++ hello.java
@@ -1,22 +1,6 @@
-pipeline{
-  agent{
-    docker {image 'jopenjdk:11' }
-    }
-    stages{
-        steps{
-            
-        }
-      stage('Checkout'){
-          steps{
-              git 'https://github.com/ali-057/java'
-          } 
-      }
-      stage('Compile and  run java program')  {
-        steps  {
-          sh 'javac hello.java'
-          sh 'java hello'
-        }
-      }
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
 
